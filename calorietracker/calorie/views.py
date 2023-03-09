e2df16b33f396198e40713865abef0ef80659f80
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-03-09 15:20:53 +0100

    dashboard toegevoegd

--- calorietracker/calorie/views.py
+++ calorietracker/calorie/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
+
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
@@ -9,3 +10,4 @@ def index(request):
 def account(request):
     template = loader.get_template('account.html')
     return HttpResponse(template.render())
+
