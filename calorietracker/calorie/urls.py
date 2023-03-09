c8b926a045de1a09b987d22c9ea9ffbbb3bc9615
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-03-09 10:52:00 +0100

    navbar toegevoed aan account pagina

--- calorietracker/calorie/urls.py
+++ calorietracker/calorie/urls.py
@@ -4,7 +4,7 @@ from . import views
 # Hier maak path's aan. Dit gebruik ik voor om de gebruiker naar een andere pagina te sturen.
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('account/', views.account, name='account'),
 ]
 
 
--- calorietracker/calorie/views.py
+++ calorietracker/calorie/views.py
@@ -5,3 +5,7 @@ from django.template import loader
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+def account(request):
+    template = loader.get_template('account.html')
+    return HttpResponse(template.render())
