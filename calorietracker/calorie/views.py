59cce8c455918ae28fcfa5395c8fb3d67c19735d
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-03-16 13:24:48 +0100

    Tracker API werkt

--- calorietracker/calorie/views.py
+++ calorietracker/calorie/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
-import json
+import os
 import requests
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
 
 
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
+
 def account(request):
     template = loader.get_template('account.html')
     return HttpResponse(template.render())
+
 
 def tracker(request):
     template = loader.get_template('tracker.html')
     return HttpResponse(template.render())
 
+
 def tracker(request):
     if request.method == 'POST':
         query = request.POST.get('query', False)
         api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
-        api_request = requests.get(
-api_url + str(query), headers={'X-Api-Key': 'YOUR-API-KEY'})        
+        api_key = os.getenv('SECRET_KEY')
+        headers = {'X-Api-Key': api_key}
+        api_request = requests.get(api_url + str(query), headers=headers)
         try:
             api = json.loads(api_request.content)
             print(api_request.content)
@@ -32,5 +40,3 @@ api_url + str(query), headers={'X-Api-Key': 'YOUR-API-KEY'})
         return render(request, 'tracker.html', {'api': api})
     else:
         return render(request, 'tracker.html', {'query': 'Enter a valid query'})
-
-    
